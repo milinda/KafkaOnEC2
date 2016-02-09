d551ffd6797d345fd049c6dd60b9082830d30f54
Milinda Pathirage <milinda.pathirage@gmail.com>
2016-02-09 12:15:00 -0500

    initial version of kafka deployment scripts (still not validated for correctness)

--- roles/kafka/templates/kafka_server_starter.sh
+++ roles/kafka/templates/kafka_server_starter.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo -e "Starting 9091 Server"
-nohup sh {{ common['soft_link_base_path'] }}/kafka/bin/kafka-server-start.sh {{ common['soft_link_base_path'] }}/kafka/config/server.properties &
+echo -e "Starting 9092 Server"
+nohup sh {{ common.soft_link_base_path }}/kafka/bin/kafka-server-start.sh {{ common.soft_link_base_path }}/kafka/config/server.properties &
